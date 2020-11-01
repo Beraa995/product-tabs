a4fa41c8b7c6484da22ecd1bc3d22b6e038f5d4c
Berin <berin@web-vision.de>
2020-11-01 17:35:17 +0100

    Add full admin crud functionality for tabs

--- Model/ResourceModel/ProductTab.php
+++ Model/ResourceModel/ProductTab.php
@@ -1,6 +1,8 @@
 <?php
 namespace BKozlic\ProductTabs\Model\ResourceModel;
 
+use BKozlic\ProductTabs\Ui\Component\Listing\Column\StoreOptions;
+use Magento\Framework\Model\AbstractModel;
 use Magento\Framework\Model\ResourceModel\Db\AbstractDb;
 
 class ProductTab extends AbstractDb
@@ -15,5 +17,38 @@ class ProductTab extends AbstractDb
     public function _construct()
     {
         $this->_init('bkozlic_product_tabs', 'tab_id');
+    }
+
+    /**
+     * Perform operations after collection save
+     *
+     * @param AbstractModel $object
+     * @return $this
+     */
+    protected function _afterSave($object)
+    {
+        $connection = $this->getConnection();
+        $storeTable = $this->getTable('bkozlic_product_tabs_store');
+        $stores = $object->getData('store_id');
+
+        if (!$stores) {
+            $stores = [StoreOptions::ALL_STORE_VIEWS];
+        }
+
+        // Delete records for tab in store table
+        $where = [
+            $this->_idFieldName . ' = ?' => (int)$object->getData($this->_idFieldName)
+        ];
+        $connection->delete($storeTable, $where);
+
+        $data = [];
+        foreach ($stores as $storeId) {
+            $data[] = [
+                $this->_idFieldName => (int)$object->getData($this->_idFieldName),
+                'store_id' => (int)$storeId,
+            ];
+        }
+        $connection->insertMultiple($storeTable, $data);
+        return $this;
     }
 }
